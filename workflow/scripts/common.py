4ea7ee48887544e2945169655c7e1c565f31e124
Marc Galland <m.galland@uva.nl>
2022-07-14 17:35:24 +0200

    add base env and function to verify barcodes

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -23,3 +23,26 @@ def sample_csv_to_pandas_df(path_to_sample_csv_file):
     
     return sample_df
 
+def verify_primer_names(barcode_fasta_file="config/barcodes.fasta"):
+    '''
+    Takes a FASTA file with two primer names and sequences (isoseq_5p and isoseq_3p)
+    Returns TRUE if 
+    '''
+    # get primer sequences
+    # Verify that it only contains two sequences
+    with open(barcode_fasta_file) as filin:
+        recs = [rec for rec in SeqIO.parse(filin, "fasta")]
+    if len(recs) == 2:
+        pass
+    else:
+        raise IndexError("Your FASTA file with barcode sequences should only contain two primers (5' and 3'). No more, no less") 
+    
+    # convert to a dictonary with id/sequence
+    # verify primer names
+    recs_dict = {rec.id: rec.seq for rec in recs}
+    if "isoseq_5p" not in recs_dict.keys():
+        raise KeyError("One primer has to be named exactly 'isoseq_5p")
+    elif "isoseq_3p" not in recs_dict.keys():
+        raise KeyError("One primer has to be named exactly 'isoseq_3p")
+    else:
+        pass
